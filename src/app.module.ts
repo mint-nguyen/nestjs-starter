79c673e76e2e795d253c936f8df86cf5e88d36a2
Mint <mintnguyen.pa@gmail.com>
2022-08-29 11:51:24 -0700

    feat: middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
+import { LoggerMiddleware } from './logger.middleware';
 import { CatsModule } from './cats/cats.module';
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -8,4 +9,8 @@ import { AppService } from './app.service';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('cats');
+  }
+}
