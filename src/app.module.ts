b5e5c41cca48acfe3a4b57b03c8a1a2bed4c7470
Mint <mintnguyen.pa@gmail.com>
2022-08-29 18:36:57 -0700

    feat: authorization

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { CatsModule } from './cats/cats.module';
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [CatsModule],
+  imports: [CatsModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
