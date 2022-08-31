855a84bbf1b68665bd9eb7c17c976412a3d59d81
Mint <mintnguyen.pa@gmail.com>
2022-08-31 13:07:41 -0700

    feat: task module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [CatsModule, AuthModule, UsersModule],
+  imports: [CatsModule, AuthModule, UsersModule, TasksModule],
   controllers: [AppController],
   providers: [AppService],
 })
