8e31036831f888db8eac0869851203cb8d9fe918
Mint <mintnguyen.pa@gmail.com>
2022-09-01 18:57:20 -0700

    feat: task entity

---
+++ src/tasks/task.entity.ts
@@ -0,0 +1,17 @@
+import { TaskStatus } from './task.model';
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Task extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  status: TaskStatus;
+}
