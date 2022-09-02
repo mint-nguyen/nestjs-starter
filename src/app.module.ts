09cf5e0c1fb41214830eb27d3341c5966f5ec136
Mint <mintnguyen.pa@gmail.com>
2022-09-01 18:40:26 -0700

    feat: typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,20 @@
 import { LoggerMiddleware } from './logger.middleware';
-import { CatsModule } from './cats/cats.module';
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [CatsModule, AuthModule, UsersModule, TasksModule],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    AuthModule,
+    UsersModule,
+    TasksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
