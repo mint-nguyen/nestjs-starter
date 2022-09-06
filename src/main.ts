5fcca8d9716ca3cdcb6b5c258ed9cd7fd5f1d05e
Mint <mintnguyen.pa@gmail.com>
2022-09-06 16:59:12 -0700

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,16 @@ import * as config from 'config';
 
 async function bootstrap() {
   const serverConfig = config.get('server');
-  const logger = new Logger('bootstrap');
-  const PORT = process.env.PORT || serverConfig.port;
 
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('bootstrap');
+  app.enableCors();
+
+  // if (process.env.NODE_ENV === 'development') {
+  //   app.enableCors();
+  // }
+  const PORT = process.env.PORT || serverConfig.port;
+
   await app.listen(PORT);
 
   logger.log(`Application starts at port ${PORT}`);
